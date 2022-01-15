3dbaf26eadd580a3c4bb62bbcb5727e6140255f9
Tracy Chan <ss.static@gmail.com>
2022-01-15 19:15:40 +1100

    Add missing files

---
+++ src/pages/tasks/Add.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+export default function Add() {
+  const [form, setForm] = useState({
+    description: "",
+    dueDate: "",
+    userName: "tracy",
+  });
+  const navigate = useNavigate();
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    const newTask = { ...form };
+
+    await fetch("http://localhost:5000/task/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+    .catch(error => {
+      window.alert(error);
+      return;
+    });
+
+    setForm({ description: "", dueDate: "", userName: "tracy" });
+    navigate("/");
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div>
+      <h3>Create New Record</h3>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="description">Description</label>
+        <input type="text"
+          id="description"
+          value={form.description}
+          onChange={(e) => updateForm({ description: e.target.value })}
+        />
+        <label htmlFor="dueDate">Due Date</label>
+        <input
+          type="date"
+          id="dueDate"
+          value={form.dueDate}
+          onChange={(e) => updateForm({ dueDate: e.target.value })}
+        />
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+  );
+}
---
+++ src/pages/tasks/Edit.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router';
+
+export default function Edit() {
+  const [form, setForm] = useState({
+    description: "",
+    dueDate: "",
+    userName: "tracy",
+    tasks: [],
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:5000/task/${params.id.toString()}`);
+
+        if (!response.ok) {
+          const message = `An error has occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+
+        const record = await response.json();
+        if (!record) {
+          window.alert(`Task with id ${id} not found`);
+          navigate("/");
+          return;
+        }
+
+        setForm(record);
+      }
+
+      fetchData();
+
+      return;
+    }, [params.id, navigate]);
+
+    // These methods will update the state properties.
+    function updateForm(value) {
+      return setForm((prev) => {
+        return { ...prev, ...value };
+      });
+    }
+
+    async function onSubmit(e) {
+      e.preventDefault();
+      const editedTask = {
+        description: form.description,
+        dueDate: form.dueDate,
+        userName: "tracy",
+      };
+
+      await fetch(`http://localhost:5000/update/${params.id}`, {
+        method: "POST",
+        body: JSON.stringify(editedTask),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+
+      navigate("/");
+    }
+
+    return (
+      <div>
+        <h3>Update Record</h3>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="description">Description: </label>
+          <input
+            type="text"
+            id="description"
+            value={form.description}
+            onChange={(e) => updateForm({ description: e.target.value })}
+          />
+          <label htmlFor="dueDate">Due Date: </label>
+          <input
+            type="date"
+            id="dueDate"
+            value={form.dueDate}
+            onChange={(e) => updateForm({ dueDate: e.target.value })}
+          />
+          <input type="submit" value="Update Task" />
+        </form>
+      </div>
+    );
+}
