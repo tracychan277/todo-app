2f914db62705588bff6b24bf45d67229311a123a
Tracy Chan <ss.static@gmail.com>
2022-01-20 10:32:31 +1100

    Add a navbar

--- react/src/App.js
+++ react/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import TaskAdd from './pages/tasks/Add';
 import TaskEdit from './pages/tasks/Edit';
 import TaskList from './pages/tasks/List';
 
 const App = () => {
- return (
-   <Router>
-     <Routes>
-       <Route exact path="/" element={<TaskList />} />
-       <Route path="/add" element={<TaskAdd />} />
-       <Route path="/edit/:id" element={<TaskEdit />} />
-     </Routes>
-   </Router>
- );
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<TaskList />} />
+        <Route path="/add" element={<TaskAdd />} />
+        <Route path="/edit/:id" element={<TaskEdit />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
